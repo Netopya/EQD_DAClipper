7bc39fc2e78be498bf09fff96125a48bff4229f6
Michael B <netopya@netopyaplanet.com>
2016-09-03 16:29:01 -0400

    Imported existing form

--- DAClipper4/DAClipper4/Form1.Designer.cs
+++ DAClipper4/DAClipper4/Form1.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parse Page";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // progressBar1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.progressBar1, 3);
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(3, 553);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1182, 14);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.progressBar1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtConsole, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1188, 570);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(389, 494);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsole.Location = new System.Drawing.Point(398, 53);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConsole.Size = new System.Drawing.Size(390, 494);
+            this.txtConsole.TabIndex = 1;
+            this.txtConsole.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(794, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(391, 494);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Analyze Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Console";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(794, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "JDownload Input";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(794, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(391, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Copy to JDownloader";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1188, 570);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EQD DA Clipper 3";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DAClipper4/DAClipper4/Form1.cs
+++ DAClipper4/DAClipper4/Form1.cs
@@ -5,16 +5,202 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
 
 namespace DAClipper4
 {
     public partial class Form1 : Form
     {
+        string clip;
+
+        private class ClipResult
+        {
+            public string status;
+            public string message;
+            
+            public int JLinksCount;
+            public string JLinks;
+            public string Title;
+
+            public IEnumerable<ParsedLink> ParsedLinks;
+            public IEnumerable<string> LinkDupes;
+            public IEnumerable<int> SourceDupes;
+            public IEnumerable<int> SourceMissing;
+        }
+
+        private class ParsedLink {
+            public string Url;
+            public string InnerText;
+            public bool Valid;
+            public bool JValid;
+            public int Source;
+
+            public ParsedLink(HtmlAgilityPack.HtmlNode node)
+            {
+                Url = node.GetAttributeValue("href","");
+                InnerText = node.InnerText;
+
+                JValid = Url.Contains("deviantart");
+
+                //Valid = !(!JValid && !InnerText.Contains("ource") || node.Descendants("img").Count() > 0);
+                Valid = (JValid && InnerText.Contains("ource")) || InnerText.Contains("ource");
+
+                Match match = Regex.Match(InnerText, @"(\d+)");
+
+                if (!int.TryParse(match.Groups[1].Value, out Source))
+                {
+                    Source = -1;
+                }
+
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            clip = Clipboard.GetText();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            if (!clip.Contains("equestriadaily.com"))
+            {
+                backgroundWorker1.ReportProgress(100, new ClipResult() { status = "error", message = "No valid Equestria Daily link found on clipboard" });
+                return;
+            }
+
+            backgroundWorker1.ReportProgress(10);
+
+            HtmlWeb website = new HtmlWeb();
+
+            HtmlAgilityPack.HtmlDocument document = website.Load(clip);
+
+            backgroundWorker1.ReportProgress(25);
+
+            //#(\d+)
+            HtmlAgilityPack.HtmlNode titleNode = document.DocumentNode.SelectSingleNode("//*[@id=\"Blog1\"]/div[1]/ul/li/div[2]/div[1]/h3/a");
+            
+            HtmlAgilityPack.HtmlNode node = document.DocumentNode.SelectSingleNode("//*[@id=\"Blog1\"]/div[1]/ul/li/div[2]/div[2]");
+            
+            IEnumerable<HtmlNode> links = node.Descendants("a");
+
+            IEnumerable<ParsedLink> parsedLinks = links.Select(l => new ParsedLink(l)).Where(v => v.Valid);
+
+            backgroundWorker1.ReportProgress(50);
+
+            IEnumerable<string> linkDupes = parsedLinks.GroupBy(l => l.Url).Where(g => g.Count() > 1).Select(ig => ig.Select(v => v.InnerText).Aggregate((a,b) => a + " and " + b));
+
+            IEnumerable<int> numeratedSources = parsedLinks.Select(l => l.Source);
+
+            IEnumerable<int> sourceDupes = numeratedSources.GroupBy(l => l).Where(g => g.Count() > 1).Select(d => d.Key).Where(n => n != -1);
+
+            backgroundWorker1.ReportProgress(75);
+
+            int max = numeratedSources.Max();
+            IEnumerable<int> sourceMissing = Enumerable.Range(1, max).Except(numeratedSources);
+
+
+            //string finalLinks = 
+
+            backgroundWorker1.ReportProgress(100, new ClipResult() 
+            { 
+                status = "success", 
+                ParsedLinks = parsedLinks, 
+                LinkDupes = linkDupes, 
+                SourceDupes = sourceDupes,  
+                SourceMissing = sourceMissing,
+                JLinksCount = parsedLinks.Where(l => l.JValid).Count(),
+                JLinks = parsedLinks.Where(l => l.JValid).Select(j => j.Url).Aggregate((a,b) => string.Format("{0}{1}{2}", a, System.Environment.NewLine, b)),
+                Title = titleNode.InnerText
+            });
+
+
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //MessageBox.Show((String)e.UserState);
+
+            progressBar1.Value = e.ProgressPercentage;
+
+            if (e.ProgressPercentage != 100) return;
+
+            button1.Enabled = true;
+            txtConsole.Text = "";
+
+            ClipResult result = (ClipResult)e.UserState;
+
+            if (result.status != "success")
+            {
+                
+                txtConsole.AppendText(result.status + System.Environment.NewLine);
+                txtConsole.AppendText(result.message);
+                return;
+            }
+
+            foreach (ParsedLink link in result.ParsedLinks)
+            {
+                richTextBox1.SelectionColor = link.JValid ? Color.Green : Color.Red;
+                richTextBox1.AppendText(link.InnerText + " " + link.Url + System.Environment.NewLine);
+            }
+
+            txtConsole.AppendText("Results for " + result.Title + System.Environment.NewLine + System.Environment.NewLine);
+            
+            txtConsole.AppendText("Links Analyzed: " + result.ParsedLinks.Count() + System.Environment.NewLine);
+            txtConsole.AppendText("Valid JDownload Links found: " + result.JLinksCount + System.Environment.NewLine + System.Environment.NewLine);
+
+            if(result.LinkDupes.Count() > 0){
+                txtConsole.AppendText(string.Format("Duplicate links ({0}) found at:{1}", result.LinkDupes.Count(), System.Environment.NewLine));
+                foreach (string dupe in result.LinkDupes)
+                {
+                    txtConsole.AppendText("    " + dupe + System.Environment.NewLine);
+                }
+            }
+
+            if (result.SourceDupes.Count() > 0)
+            {
+                txtConsole.AppendText(string.Format("Source numbers duplicated ({0}):{1}", result.SourceDupes.Count(), System.Environment.NewLine));
+                foreach (int dupe in result.SourceDupes)
+                {
+                    txtConsole.AppendText(string.Format("    Source[{0}]{1}", dupe, System.Environment.NewLine));
+                }
+            }
+
+            if (result.SourceMissing.Count() > 0)
+            {
+                txtConsole.AppendText(string.Format("The following source numbers ({0}) could not be found:{1}", result.SourceMissing.Count(), System.Environment.NewLine));
+                foreach (int missing in result.SourceMissing)
+                {
+                    txtConsole.AppendText(string.Format("    Source[{0}]{1}", missing, System.Environment.NewLine));
+                }
+            }
+
+            textBox2.Text = result.JLinks;
+            
+            txtConsole.AppendText(System.Environment.NewLine + "Thank you for using EQD DA Clipper!");
+
+            //IEnumerable<ParsedLink> ParsedLinks = (IEnumerable<ParsedLink>)e.UserState;
+
+           // string outText = string.Join(System.Environment.NewLine, ParsedLinks.Select(l => l.InnerText + " " + l.Source + " " + l.Url));
+
+            //txtConsole.Text = outText;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
         }
     }
 }
